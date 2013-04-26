9ff6e30944d551cc7d20a9d58ee06ee612761b97
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-04-26 16:24:39 +0200

    Added basic functionality to Index class

--- grape/index.py
+++ grape/index.py
@@ -5,7 +5,7 @@ class Metadata(object):
     """A class to store metadata retrieved from indices
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, kwargs):
         for key in kwargs:
             if key in self.__dict__.keys():
                 raise ValueError("%r already contains %r property" % (self.__class__,key))
@@ -28,11 +28,49 @@ class Metadata(object):
         value = self.get(key)
         return sep.join([key,str(value)])+trail
 
+class IndexType():
+    META = 0
+    DATA = 1
+
+    @classmethod
+    def get(cls, str):
+        return {
+            'META' : cls.META,
+            'DATA' : cls.DATA,
+            cls.META : 'META',
+            cls.DATA : 'DATA'
+        }.get(str,None)
+
+
 class Index(object):
     """A class to access information stored into index files
     """
 
-    def __init__(self, path):
+    def __init__(self, path, type=IndexType.DATA, entries=[]):
         self.path = path
+        self.type = type
 
+        self.entries = entries
 
+    def initialize(self):
+        with open(self.path, 'r') as index_file:
+            for line in index_file:
+                self._parse_line(line)
+
+    def _parse_tags(self, str, tagsep=" ", pairsep='=', pairtrail=';'):
+        tags = {}
+        blocks = str.split(' ')
+        for block in blocks:
+            tag = self._parse_tag(block, sep=pairsep, trail=pairtrail)
+            tags[tag[0]] = tag[1]
+        return tags
+
+    def _parse_tag(sef, str, sep='=', trail=';'):
+        pair = str.split(sep)
+        return [pair[0], pair[1][:-len(trail)]]
+
+    def _parse_line(self, line):
+        entry = line.split('\t')
+        tags = self._parse_tags(entry[1])
+
+        self.entries.append({'file': entry[0], 'metadata': Metadata(tags)})
