bb1c68a930d542fb41dee58b19f52fd73e23ff1e
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-05-16 18:12:05 +0200

    Removed old code from project test - doing nothing for now

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -8,11 +8,12 @@ from grape import Project
 def test_project_datasets_flat():
     p = Project("test_data/project_flat")
     datasets = p.get_datasets()
-    assert len(datasets) == 2
-    first = filter(lambda x: x.name == "first", datasets)[0]
-    print first.primary
-    assert first.primary.endswith("first_1.fastq")
-    assert first.data_folder.endswith("project_flat/data")
-    print first.type_folders == False
+    assert len(datasets) == 1
+    #assert len(datasets.fastq) == 2
+    #first = filter(lambda x: x.name == "first", datasets)[0]
+    #print first.primary
+    #assert first.primary.endswith("first_1.fastq")
+    #assert first.data_folder.endswith("project_flat/data")
+    #print first.type_folders == False
 
 
