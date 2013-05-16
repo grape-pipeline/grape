4e4d621e0fb24e893869f2b789fcb4fd157c37d7
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-05-16 15:42:54 +0200

    Changed some pipeline parameters with index file values

--- grape/pipelines.py
+++ grape/pipelines.py
@@ -17,21 +17,24 @@ def default_pipeline(dataset, config=None):
     if config is None:
         config = {}
 
-    index = config.get("index", None)
-    annotation = config.get("annotation", None)
+    index = config.get("genome")
+    annotation = config.get("annotation")
+    quality = config.get("quality")
 
     if index is None:
-        index = dataset.get_index()
+        index = dataset.get_index(config)
     if annotation is None:
-        annotation = dataset.get_annotation()
+        annotation = dataset.get_annotation(config)
+    if quality is None:
+        quality = dataset.quality
 
-    pipeline = Pipeline(name="Default Pipeline %s" % (dataset.name))
+    pipeline = Pipeline(name="Default Pipeline %s" % (dataset.id))
     gem = pipeline.add(tools.gem())
     gem.index = index
     gem.annotation = annotation
-    gem.quality = dataset.quality
+    gem.quality = quality
     gem.output_dir = dataset.folder("mappings")
-    gem.name = dataset.name
+    gem.name = dataset.id
     gem.primary = dataset.primary
     if not dataset.single_end:
         gem.secondary = dataset.secondary
@@ -39,6 +42,6 @@ def default_pipeline(dataset, config=None):
     flux = pipeline.add(tools.flux())
     flux.annotation = annotation
     flux.input = gem.bam
-    flux.name = dataset.name
+    flux.name = dataset.id
     flux.output_dir = dataset.folder("quantifications")
     return pipeline
