1dea983bc486c492d0afc67f0ecb84051f0c0510
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-08-29 17:04:13 +0200

    Add function to get file statistics

--- grape/utils.py
+++ grape/utils.py
@@ -17,6 +17,14 @@ def uni_convert(input):
     else:
         return input
 
+def file_stats(path, human_readable=False):
+    import os
+    md5 = md5sum(path)
+    size = os.path.getsize(path)
+    if human_readable:
+        size = sizeof_fmt(size)
+    return (md5,size)
+
 def md5sum(filename, n_blocks=128):
     import hashlib
     md5 = hashlib.md5()
@@ -25,3 +33,17 @@ def md5sum(filename, n_blocks=128):
             md5.update(chunk)
     return md5.hexdigest()
 
+from math import log
+unit_list = zip(['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'], [0, 0, 1, 2, 2, 2])
+def sizeof_fmt(num):
+    """Human friendly file size"""
+    if num > 1:
+        exponent = min(int(log(num, 1024)), len(unit_list) - 1)
+        quotient = float(num) / 1024**exponent
+        unit, num_decimals = unit_list[exponent]
+        format_string = '{:.%sf}{}' % (num_decimals)
+        return format_string.format(quotient, unit)
+    if num == 0:
+        return '0 bytes'
+    if num == 1:
+        return '1 byte'
