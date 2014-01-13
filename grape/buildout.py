168e5ad2d0e67fc4c4eb5a4fcdbcaf4bde0a2192
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-01-13 14:26:44 +0100

    Leave job environment as default and use absolute path for binaries instead

--- grape/buildout.py
+++ grape/buildout.py
@@ -106,26 +106,8 @@ class module(object):
 
     def __call__(self, *args):
         cl = args[0]
-        try:
-            old_validate = cl.validate
-        except:
-            old_validate = lambda x: True
         cl.modules = self.modules
         cl._load_modules = self._load_modules
-
-        def validate(self):
-            old_validate(self)
-            if not self.job.env:
-                self.job.env = {}
-            path = [p for p in self.job.env.get('PATH', '${PATH}').split(':') if p is not '']
-            modules_path = self._load_modules()
-            for module_path in modules_path:
-                module_path = os.path.join(module_path,'bin')
-                if module_path not in path:
-                    path.insert(0, module_path)
-            self.job.env['PATH'] = ':'.join(path)
-
-        cl.validate = validate
 
         return cl
 
