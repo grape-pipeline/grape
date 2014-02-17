4e73629441c3e8a262559dc70589d34b37eb56c6
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-02-17 16:19:12 +0100

    Add reverse mate tool

--- grape/tools.py
+++ grape/tools.py
@@ -288,6 +288,36 @@ class awk_fix_se(object):
         command = "\'BEGIN{OFS=FS=\"\\t\"}$0!~/^@/{split(\"1_2_8_32_64_128\",a,\"_\");for(i in a){if(and($2,a[i])>0){$2=xor($2,a[i])}}}{print}\'"
         return 'bash','awk %s ${input|arg("")|suf(" ")}${output|arg("> ")}' % command
 
+
+@module([("crgtools","0.1")])
+@tool('grape_reverse_mate')
+class awk_reverse_mate(object):
+    """\
+    An AWK script for stranded data to reverse a mate in a SAM file.
+
+    Usage:
+        reverse_mate -i <input> -o <output> [-n <name>] [--second-mate]
+
+    Options:
+        --help  Show this help message
+        -n, --name <name>  The output prefix name
+        -o, --output <output>  The output file [default: stdout]
+        --second-mate  Reverse the second mate. [default: false]
+
+    Inputs:
+        -i, --input <input>  The input map file [default: stdin]
+    """
+    def setup(self):
+        if self.options['name']:
+            self.name("awk_reverse_mate.${name}")
+            self.options['name'].hidden = True
+        self.add_option('mate', '128' if self.options['second_mate'] else '64')
+
+    def get_command(self):
+        command = "\'BEGIN {OFS=\"\\t\"} {if ($1!~/^@/ && and($2,${mate})>0) {$2=xor($2,0x10)}; print}\'"
+        return 'bash','awk %s ${input|arg("")|suf(" ")}${output|arg("> ")}' % command
+
+
 @module([("samtools","0.1.19")])
 @tool('grape_samtools_view')
 class samtools(object):
